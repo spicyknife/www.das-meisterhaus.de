70801e44cb733101d904702dd2c5a44493088a96
spicyknife <spicyknife@web.de>
2024-06-26 16:48:10 +0200

    Mail PHP nach Konfigurator

--- api/mail.php
+++ api/mail.php
@@ -7,15 +7,30 @@
 if(isset($postdata) && !empty($postdata))
 {
   // Extract the data.
-  $request = json_decode($postdata);
+  $request = json_decode($postdata, true);
 
   // Validate.
     header("Access-Control-Allow-Origin: *");
     http_response_code(201);
     $result = '';
     foreach ($request as $key => $value) {
+      if (is_array($value) && !array_is_list($value)) {
+        foreach ($value as $innerKey => $innerValue) {
+            if (is_array($innerValue) && !array_is_list($innerValue)) {
+                foreach ($innerValue as $moreInnerKey => $moreInnerValue) {
+                    $result .= "$moreInnerKey: $moreInnerValue <br>";
+                }
+            }
+            else {
+              $result .= "$innerKey: $innerValue <br>";
+            }
+        }
+    }
+    else {
       $result .= "$key: $value <br>";
     }
+    }
+
     mail($mail,$subject, $result, $from);
 
 }
